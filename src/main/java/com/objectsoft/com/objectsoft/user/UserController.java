cdec42b12ff2eb8e4a9b8c963ef852cfccf15471
hdossani <hiren.dossani@gmail.com>
2019-10-14 08:54:59 -0400

    First Commit

---
+++ src/main/java/com/objectsoft/com/objectsoft/user/UserController.java
@@ -0,0 +1,13 @@
+package com.objectsoft.com.objectsoft.user;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World";
+    }
+}
---
+++ src/test/java/com/objectsoft/SpringBootDemoApplicationTest.java
@@ -0,0 +1,27 @@
+package com.objectsoft;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+
+public class SpringBootDemoApplicationTest {
+
+    @Test
+    public void contextLoads(){
+
+    }
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
